410d7e621bcadf5ba745d55b8a70d7ac0eeb332c
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2024-03-02 16:58:19 +0900

    next.config.mjsの変更: outputを'export'に設定し、環境変数を追加

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   trailingSlash: true,
-  output: "export"
+  env: {
+    MICROCMS_API_KEY: process.env.MICROCMS_API_KEY,
+    MICROCMS_SERVICE_DOMAIN: process.env.MICROCMS_SERVICE_DOMAIN,
+  },
 };
 
 export default nextConfig;
