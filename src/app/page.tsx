03b53601b40cff65fd8353c6378b1e3e370abc20
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2024-03-02 19:48:29 +0900

    CMSBlogs型追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,25 @@
+import { CMSBlogs } from "@/types";
 import axios from "axios";
+
 
 const Page = async () => {
 
-  const blog = await axios.get(`https://${process.env.MICROCMS_SERVICE_DOMAIN}.microcms.io/api/v1/blogs`, {
+  const blogs: CMSBlogs | null = await axios.get(`https://${process.env.MICROCMS_SERVICE_DOMAIN}.microcms.io/api/v1/blogs`, {
     headers: { 'X-MICROCMS-API-KEY': process.env.MICROCMS_API_KEY }
   }).then((res) => {
     return res.data;
+  }).catch((error) => {
+    return null;
   });
 
   return (
     <div>
       <h1>Page</h1>
+      <ul>
+        {blogs && blogs.contents.map((blog) => (
+          <li key={blog.id}>{blog.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
