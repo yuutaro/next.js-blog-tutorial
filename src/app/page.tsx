6982918d04f9ce9cdba24a19e43fb83b83936d10
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2024-03-02 16:59:01 +0900

    とりあえずaxiosでapiを受け取れるようになった（要リファクタリング）

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
-export default function Home() {
+import axios from "axios";
+
+const Page = async () => {
+
+  const blog = await axios.get(`https://${process.env.MICROCMS_SERVICE_DOMAIN}.microcms.io/api/v1/blogs`, {
+    headers: { 'X-MICROCMS-API-KEY': process.env.MICROCMS_API_KEY }
+  }).then((res) => {
+    return res.data;
+  });
+
   return (
-    <main>
-      <p className="bg-slate-50 h-screen w-full">Hello, world!</p>
-    </main>
+    <div>
+      <h1>Page</h1>
+    </div>
   );
 }
+
+export default Page;
