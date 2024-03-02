29d081a14f32b44b36012b4459d750ca3ed9b5c1
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2024-03-02 16:57:37 +0900

    libs dir => lib dir

---
+++ lib/client.ts
@@ -0,0 +1,15 @@
+
+import { createClient } from "microcms-js-sdk";
+
+if (!process.env.MICROCMS_SERVICE_DOMAIN) {
+  throw new Error("Please define MICROCMS_SERVICE_DOMAIN in .env.local")
+}
+
+if (!process.env.MICROCMS_API_KEY) {
+  throw new Error("Please define MICROCMS_API_KEY in .env.local")
+}
+
+export const client = createClient({
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: process.env.MICROCMS_API_KEY
+})
--- libs/client.js
+++
@@ -1,7 +0,0 @@
-import { error } from "console";
-import { createClient } from "microcms-js-sdk";
-
-export const client = createClient({
-  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN || error("MICROCMS_SERVICE_DOMAIN is not defined"),
-  apiKey: process.env.MICROCMS_API_KEY || error("MICROCMS_API_KEY is not defined"),
-})
