5d07ca2227b27dfd78ac50c5671a7d4814b58e6f
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2024-03-02 13:29:47 +0900

    SSG設定変更 https://zenn.dev/yamadadayo123/articles/6cb4f586de0183#ssg%E3%81%AE%E8%A8%AD%E5%AE%9A%E6%96%B9%E6%B3%95%E3%81%8C%E5%A4%89%E3%82%8F%E3%81%A3%E3%81%9F

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true
+  trailingSlash: true,
+  output: "export"
 };
 
 export default nextConfig;
